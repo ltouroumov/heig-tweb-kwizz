ba01b4a07aa8bbbf732475d53362315e1ce240b7
Laureline David <ltouroumov@gmail.com>
2017-01-16 16:16:38 +0100

    Changed API app

--- KwizzApi/Program.cs
+++ KwizzApi/Program.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using KwizzApi;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
-namespace WebApplication
+namespace KwizzApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
